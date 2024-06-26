a4c5fb8db35c92d86e4e27309e0fefa14a8b26e9
DevOps <devops@DevOpss-MacBook-Pro.local>
2024-06-26 00:29:53 -0700

    Add_new

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -36,7 +36,7 @@ public class NumericController {
 			if (value > 50) {
 				message = "Greater than 50";
 			} else {
-				message = "Smaller than or equal to 5 password:123456";
+				message = "Smaller than or equal to 5";
 			}
 
 			return message;
